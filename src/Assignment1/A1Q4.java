98c79a39f186899b2dd11f5407c3d8bc0b27542b
Serrs9964 <Serrs9964@HRH-IC0027321.wrdsb.ca>
2017-09-19 13:38:58 -0400

    Robot in kitchener assignment A1Q1-2

--- src/Assignment1/A1Q4.java
+++ src/Assignment1/A1Q4.java
@@ -4,6 +4,11 @@
  */
 package Assignment1;
 
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Wall;
+
 /**
  *
  * @author serrs9964
@@ -15,5 +20,27 @@ public class A1Q4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        City kitchener = new City();
+        new Wall(kitchener, 0, 1, Direction.WEST);
+        new Wall(kitchener, 1, 1, Direction.WEST);
+        new Wall(kitchener, 1, 1, Direction.SOUTH);
+        Robot Karel = new Robot(kitchener, 0,0, Direction.SOUTH);
+        Robot Lerak = new Robot(kitchener, 0,1, Direction.SOUTH);
+        Karel.move();
+        Lerak.move();
+        Karel.move();
+        Lerak.turnLeft();
+        Lerak.move();
+        Karel.turnLeft();
+        Lerak.turnLeft();
+        Lerak.turnLeft();
+        Lerak.turnLeft();
+        Lerak.move();
+        Karel.move();
+        Lerak.turnLeft();
+        Lerak.turnLeft();
+        Lerak.turnLeft();
+        Lerak.move();
+        
     }
 }
